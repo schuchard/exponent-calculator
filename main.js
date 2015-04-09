ad1d78a3b2de4b9da56dd3e6dee5ea325dbc7aae
schuchard <kbs177@gmail.com>
2015-04-09 17:24:11 -0600

    add table, add responsive design, add each calculation to form

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 // Use the techniques shown so far to write a program that calculates and shows the value of (2 to the 10th power)
 
+/**
+ * [exponentCalculator description]
+ * @param  {Number} num [number to be multiplied]
+ * @param  {Number} pow [number of times to multiply number by itself]
+ * @return {Number}     [the solution from the multiplying]
+ */
 var exponentCalculator = function (num, pow){
   var number = num;
   for (var i = 1; i < pow; i++) {
@@ -8,17 +14,32 @@ var exponentCalculator = function (num, pow){
   return number;
 };
 
+var addRow = function(count, num, pow, ans) {
+  var counter = ('<td>' + count + '</td>');
+  var number = ('<td>' + num + '</td>');
+  var power = ('<td>' + pow + '</td>');
+  var answer = ('<td>' + ans + '</td>');
+
+  return $('.answerTable tr').last().after('<tr>' + counter + number + power + answer + '</tr>');
+};
 
 $(document).on('ready', function() {
+var count = 0;
+// Run calculation on form submit
 $('.expCalc').on('submit', function(e){
   $('.answer').fadeOut('fast');
   e.preventDefault();
+    // Pause for previous number to fadeOut
     setTimeout(function(){
     var num = $('#number').val();
     var pow = $('#power').val();
-    $('.answer').text(exponentCalculator(num, pow)).slideDown('fast');
+    var ans = exponentCalculator(num, pow);
+    $('.answer').text(ans).slideDown('fast');
     $('.expCalc')[0].reset();
+    count++;
+    addRow(count, num, pow, ans);
     },300);
+
 });
 
 });
