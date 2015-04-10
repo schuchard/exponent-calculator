d958635c09f558073f5b4567a59ae59d4ee8e331
schuchard <kbs177@gmail.com>
2015-04-09 18:28:39 -0600

    add running total, error handling for non-numbers

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 // Use the techniques shown so far to write a program that calculates and shows the value of (2 to the 10th power)
 
 /**
- * [exponentCalculator description]
- * @param  {Number} num [number to be multiplied]
- * @param  {Number} pow [number of times to multiply number by itself]
- * @return {Number}     [the solution from the multiplying]
+ * [exponentCalculator - Functions as an exponential calculator]
+ * @param  {Number} num [The number to be multiplied]
+ * @param  {Number} pow [The number of times to multiply the number by itself]
+ * @return {Number}     [The solution from the multiplying]
  */
 var exponentCalculator = function (num, pow){
   var number = num;
   for (var i = 1; i < pow; i++) {
     number *= num;
   }
-  return number;
+  return parseFloat(number);
 };
 
+/**
+ * [addRow - Returns a jQuery row]
+ * @param {Number} count [The calculation number]
+ * @param {Number} num   [The number to be multiplied]
+ * @param {Number} pow   [The number of times to multiply the number by itself]
+ * @param {Number} ans   [The Answer]
+ */
 var addRow = function(count, num, pow, ans) {
   var counter = ('<td>' + count + '</td>');
   var number = ('<td>' + num + '</td>');
@@ -25,19 +32,51 @@ var addRow = function(count, num, pow, ans) {
 
 $(document).on('ready', function() {
 var count = 0;
+var total = 0;
+var showTotal = false;
+
 // Run calculation on form submit
 $('.expCalc').on('submit', function(e){
+  // Clear out previous number after first calculation has been made
   $('.answer').fadeOut('fast');
   e.preventDefault();
+
     // Pause for previous number to fadeOut
     setTimeout(function(){
     var num = $('#number').val();
     var pow = $('#power').val();
     var ans = exponentCalculator(num, pow);
-    $('.answer').text(ans).slideDown('fast');
+
+    // Reset form
     $('.expCalc')[0].reset();
-    count++;
-    addRow(count, num, pow, ans);
+
+
+    if(isNaN(ans)){
+      $('.error').fadeToggle();
+      setTimeout(function(){
+        $('.error').fadeToggle();
+      },1500);
+    }
+    else {
+      // Increment counter on each submit
+      count++;
+      // Add answer to page
+      $('.answer').text(ans).slideDown('fast');
+      // Add table row of each calculation
+      addRow(count, num, pow, ans);
+
+      // Add each calculation to running total
+      var runningTotal = total += ans;
+
+      // Show running total after first calculation
+      if(!showTotal){
+        $('.runningTotal').fadeToggle();
+        showTotal = true;
+      }
+      // Set running total
+      $('.total').text(runningTotal);
+    }
+
     },300);
 
 });
